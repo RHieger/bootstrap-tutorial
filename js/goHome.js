97fdf38dffa10598b8ba57eefe2d50a84a6b0f49
Bobby <rhieger@nyc.rr.com>
2017-04-27 11:56:57 -0400

    Exercise 1 NOW Completed: Forgot to Stage index and goHome.js For Commit

---
+++ js/goHome.js
@@ -0,0 +1,12 @@
+// goHome.js
+// Robert Hieger
+
+// Add event listener for button with id of #home.
+
+document.getElementById('home').addEventListener('click', goHome);
+
+function goHome() {
+
+   window.location.href = '../';
+
+}  // end goHome()
